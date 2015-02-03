e3e44143b01781f79bbb85b0e4aa90b6094928f5
Brad Kaiser <bkaiser@lancope.com>
2015-02-02 22:46:12 -0500

    added lodash and got file import working

--- js/main.js
+++ js/main.js
@@ -1,30 +1,48 @@
-var app = angular.module('grouper', []);
+var app = angular.module('grouper', ['ngLodash']);
 
 app.controller('MainCtrl', function($scope) {
     $scope.selectedTab = 1;
 
-    $scope.students = ["doug","marcia","bill"];
+    $scope.model = { students: ["doug","marcia","bill"],
+                     teams: [ {name: "red", color: "#E74C3C"},
+                             {name: "orange", color: "#E67E22"},
+                             {name: "yellow", color: "#F1C40F"},
+                             {name: "green", color: "#27AE60"},
+                             {name: "aqua", color: "#1ABC9C"},
+                             {name: "blue", color: "#3498DB"},
+                             {name: "purple", color: "#9B59B6"},
+                             {name: "indigo", color: "#191970"},
+                             {name: "grey", color: "#95A5A6"},
+                             {name: "pink", color: "#DB0A5B"}]
+
+    };
 });
 
 
-app.controller('ImportCtrl', function($scope) {
+app.controller('ImportCtrl', ['$scope', 'lodash', function($scope, _) {
 
     $scope.files = [];
 
     $scope.import = function() {
-        for (var i = 0; i< $scope.files.length; i++) {
-            var file = $scope.files[i];
+        _.forEach($scope.files, function(file) {
             var fileReader = new FileReader();
 
             fileReader.onload = function(e) {
-                console.log(fileReader.result);
-            }
+                $scope.$apply(function() {
+                    var names = _(fileReader.result.split("\n"))
+                                    .map(function(line) { return line.split(",") })
+                                    .pluck(1)
+                                    .value();
+
+                    $scope.model.students = names;
+                    $scope.files = [];
+                });
+            };
 
             fileReader.readAsText(file);
-        }
-    }
-
-});
+        });
+    };
+}]);
 
 app.controller('TeamCtrl', function($scope) {
 });
