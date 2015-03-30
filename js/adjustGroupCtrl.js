831120e530dfc90f756817dfb066b4d3d4c92480
dhatch6 <donovan.hatch@gmail.com>
2015-03-30 11:46:38 -0400

    Updated adjust to work with new format. only works with Adityas for now

--- js/adjustGroupCtrl.js
+++ js/adjustGroupCtrl.js
@@ -2,14 +2,14 @@ angular.module('grouper')
     .controller('AdjustGroupCtrl', ['$scope', 'dataService', function($scope, dataService) {
 
         $scope.layout = 0; //0 == list, 1 == visual
-        $scope.selectedGroup = "Group 0";
+        $scope.selectedGroup = 0;
 
         $scope.test = function(id) {
             $scope.selectedGroup = id;
         }
         $scope.onDropComplete=function(data,event, gIndex){
             var res = data.split(':');
-            var item = $scope.model.groups[res[0]].splice(res[1], 1)[0];
-            $scope.model.groups[gIndex].push(item);
+            var item = $scope.model.groups[res[0]].items.splice(res[1], 1)[0];
+            $scope.model.groups[gIndex].items.push(item);
         }
 }]);
